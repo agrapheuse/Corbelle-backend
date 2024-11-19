66cad18bbb84cbdc3d3b85832388c16de8d1e362
agrapheuse <noah.diderich@gmail.com>
2024-11-19 20:09:35 +0100

    get base path

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -6,6 +6,10 @@ export default function registerRoutes(app: Application): void {
   const imageController = new ImageController();
   const imageRoutes = imageController.routes();
 
+  app.get("/", (_req, res) => {
+    res.send("Welcome to the Image API!");
+  });
+
   // Register routes dynamically
   imageRoutes.forEach((route) => {
     app[route.method](imageController.basePath + route.path, route.handler);
